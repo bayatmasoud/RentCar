b781ab4c4d1239e91707800114b00f57ee078b89
bayat.masoud@gmail.com <bayat.masoud@gmail.com>
2021-07-22 15:19:54 +0300

    Suggestion Change Icons

--- lib/Presentation/Cards/SuggestionCard.dart
+++ lib/Presentation/Cards/SuggestionCard.dart
@@ -5,8 +5,8 @@ class SuggestionCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 7),
-      height: MediaQuery.of(context).size.height * 0.08,
-      width: MediaQuery.of(context).size.width * 0.95,
+      height: MediaQuery.of(context).size.height * 0.1,
+      width: MediaQuery.of(context).size.width * 0.98,
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15.0),
@@ -16,42 +16,46 @@ class SuggestionCard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Tooltip(
-              message: 'Cheapest Cars',
-              child:
-                  IconButton(icon: Icon(Icons.attach_money), onPressed: () {}),
-            ),
-            Tooltip(
-              message: 'Recommanded Car',
-              child: IconButton(
-                icon: Icon(
-                  Icons.recommend,
-                  color: Colors.amber[800],
+                message: 'Cheapest Cars',
+                child: Image.asset(
+                  'asset/images/Cheapest.png',
+                  height: 40,
+                  width: 40,
+                )
+                // IconButton(icon: Icon(Icons.attach_money), onPressed: () {}),
                 ),
-                onPressed: () {},
-              ),
-            ),
+            Tooltip(
+                message: 'Recommanded Car',
+                child: Image.asset(
+                  'asset/images/recommanded.png',
+                  height: 50,
+                  width: 50,
+                )),
             Tooltip(
               message: 'Nearest Car',
               child: IconButton(
-                icon: Icon(Icons.gps_fixed),
+                icon: Icon(
+                  Icons.gps_fixed,
+                  size: 40,
+                ),
                 onPressed: () {},
                 hoverColor: Colors.red,
               ),
             ),
             Tooltip(
                 message: 'Electric Car',
-                child: IconButton(
-                    icon: Icon(Icons.electric_car_sharp), onPressed: () {})),
+                child: Image.asset(
+                  'asset/images/electric.png',
+                  height: 40,
+                  width: 40,
+                )),
             Tooltip(
-              message: 'This Cars Has Insurance',
-              child: IconButton(
-                  icon: Icon(Icons.account_balance_wallet_rounded),
-                  onPressed: () {}),
-            ),
-            Tooltip(
-                message: '5 Star Security',
-                child:
-                    IconButton(icon: Icon(Icons.security), onPressed: () {})),
+                message: 'Luxury Cars',
+                child: Image.asset(
+                  'asset/images/luxury.png',
+                  height: 40,
+                  width: 40,
+                )),
           ],
         ),
       ),
